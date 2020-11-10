f6495d1c974f87d6949e800a886e3ff12c681b57
Utkarsh Jaiswal <utkarshjaiswal54@gmail.com>
2020-11-09 20:04:39 -0500

    Testing node creation

--- rdma/compute_node.cc
+++ rdma/compute_node.cc
@@ -1,36 +1,50 @@
 #include <iostream>
 #include "rdma.h"
+#include "../test/test_suite.h"
+
+/*
+ * GetEmptyBTree() - Returns an empty Btree multimap object created on the heap
+ */ 
+BTreeType *GetEmptyBTree() {
+  BTreeType *t = new BTreeType{KeyComparator{1}};
+  
+  return t; 
+}
 
 void client_thread(RDMA_Manager* rdma_manager){
+
+  BTreeType *root = GetEmptyBTree();
+  int key_num = 30 * 1024 * 1024;
+  printf("Using key size = %d (%f million)\n",
+           key_num,
+           key_num / (1024.0 * 1024.0));
 
   auto myid = std::this_thread::get_id();
   std::stringstream ss;
   ss << myid;
   std::string thread_id = ss.str();
+
+  long int start_key = key_num / 40 * (long)1;
+
+  root->insert(start_key, start_key);
+
   rdma_manager->Remote_Memory_Register(100*1024*1024);
   rdma_manager->Remote_Query_Pair_Connection(thread_id);
   std::cout << rdma_manager->remote_mem_pool[0];
   ibv_mr mem_pool_table[2];
+  rdma_manager->local_mem_pool[0] = (ibv_mr *)root;
   mem_pool_table[0] = *(rdma_manager->local_mem_pool[0]);
   mem_pool_table[1] = *(rdma_manager->local_mem_pool[0]);
-  mem_pool_table[1].addr = (void*)((char*)mem_pool_table[1].addr + sizeof("message from computing node"));// PROBLEM Could be here.
+  mem_pool_table[1].addr = (void*)((char*)mem_pool_table[1].addr + sizeof("root"));// PROBLEM Could be here.
 
-  char *msg = static_cast<char *>(rdma_manager->local_mem_pool[0]->addr);
-  strcpy(msg, "message from computing node");
-  int msg_size = sizeof("message from computing node");
+  // char *msg = static_cast<char *>(rdma_manager->local_mem_pool[0]->addr);
+  // strcpy(msg, "message from computing node");
+  int msg_size = sizeof(root);
   rdma_manager->RDMA_Write(rdma_manager->remote_mem_pool[0], &mem_pool_table[0],
                            msg_size, thread_id);
 
   rdma_manager->RDMA_Read(rdma_manager->remote_mem_pool[0], &mem_pool_table[1],
                           msg_size, thread_id);
-//  ibv_wc* wc = new ibv_wc();
-//
-//    rdma_manager->poll_completion(wc, 2, thread_id);
-//    if (wc->status != 0){
-//      fprintf(stderr, "Work completion status is %d \n", wc->status);
-//      fprintf(stderr, "Work completion opcode is %d \n", wc->opcode);
-//    }
-
 
   std::cout << "write buffer: " << (char*)mem_pool_table[0].addr << std::endl;
 
